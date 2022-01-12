cb783f7d50645414028a5f0a7fae41a9d17c2f6a
renato3x <pereirarenato21@gmail.com>
2022-01-12 20:51:26 -0300

    MainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,22 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   @Override
   public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+    Parent root = loader.load();
+    Scene scene = new Scene(root);
 
+    stage.setTitle("Sample JavaFX Application");
+    stage.setScene(scene);
+
+    stage.show();
   }
 
   public static void main(String[] args) {
