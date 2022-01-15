f3926d3a9206167dbf022a49b3d0ae30aad9cd3a
renato3x <pereirarenato21@gmail.com>
2022-01-15 12:12:59 -0300

    Initialing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -16,6 +16,7 @@ import model.services.DepartmentService;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 public class MainViewController implements Initializable {
 
@@ -35,12 +36,15 @@ public class MainViewController implements Initializable {
 
   @FXML
   public void onMenuItemDepartmentAction() {
-    loadView2("/gui/DepartmentList.fxml");
+    loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+      controller.setService(new DepartmentService());
+      controller.updatedTableView();
+    });
   }
 
   @FXML
   public void onMenuItemAboutAction() {
-    loadView("/gui/About.fxml");
+    loadView("/gui/About.fxml", x -> {});
   }
 
   @Override
@@ -48,24 +52,7 @@ public class MainViewController implements Initializable {
 
   }
 
-  private synchronized void loadView(String absoluteName) {
-    try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-      VBox vbox = loader.load();
-
-      Scene mainScene = Main.getMainScene();
-      VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-
-      Node mainMenu = mainVbox.getChildren().get(0);
-      mainVbox.getChildren().clear();
-      mainVbox.getChildren().add(mainMenu);
-      mainVbox.getChildren().addAll(vbox.getChildren());
-    } catch (IOException e) {
-      Alerts.showAlert("IO Expection", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
-    }
-  }
-
-  private synchronized void loadView2(String absoluteName) {
+  private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
     try {
       FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
       VBox vbox = loader.load();
@@ -78,9 +65,7 @@ public class MainViewController implements Initializable {
       mainVbox.getChildren().add(mainMenu);
       mainVbox.getChildren().addAll(vbox.getChildren());
 
-      DepartmentListController controller = loader.getController();
-      controller.setService(new DepartmentService());
-      controller.updatedTableView();
+      initializingAction.accept(loader.getController());
     } catch (IOException e) {
       Alerts.showAlert("IO Expection", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
     }
