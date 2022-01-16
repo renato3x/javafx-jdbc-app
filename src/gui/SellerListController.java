158b91f4d5781b23c3c50a8a064952006f10800e
renato3x <pereirarenato21@gmail.com>
2022-01-16 10:55:02 -0300

    Seller TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -23,6 +23,7 @@ import model.services.SellerService;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -41,6 +42,15 @@ public class SellerListController implements Initializable, DataChangeListener {
 
   @FXML
   private TableColumn<Seller, String> tableColumnName;
+
+  @FXML
+  private TableColumn<Seller, String> tableColumnEmail;
+
+  @FXML
+  private TableColumn<Seller, Date> tableColumnBirthDate;
+
+  @FXML
+  private  TableColumn<Seller, Double> tableColumnBaseSalary;
 
   @FXML
   private TableColumn<Seller, Seller> tableColumnEDIT;
@@ -65,6 +75,12 @@ public class SellerListController implements Initializable, DataChangeListener {
   private void initializeNodes() {
     tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
     tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+    tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+    tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+    Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+    tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+    Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
+
 
     Stage stage = (Stage) Main.getMainScene().getWindow();
 
